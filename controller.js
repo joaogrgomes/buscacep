3a63522a5dfa82803eb9e9869d32e6a99fcbedfd
Joao Guilherme <joaogrgomes@gmail.com>
2021-03-31 01:49:54 -0300

    Adiciona execução via tecla 'enter'

--- controller.js
+++ controller.js
@@ -2,31 +2,61 @@ var submitButton = document.querySelector('#app button')
 var zipCodeField = document.querySelector('#app input')
 var content = document.querySelector('#app main')
 
-submitButton.addEventListener('click', run)
 
-function run (event) {
-    event.preventDefault()
 
-    var zipcode = zipCodeField.value
 
-    zipcode = zipcode.replace(' ', '')
-    zipcode = zipcode.replace('.', '')
-    zipcode = zipcode.trim()
+if (zipCodeField && submitButton)    {
+    zipCodeField.addEventListener("keypress", function (e) {
+        if (event.keyCode === 13)   {
+            event.preventDefault()
 
-    axios
-    .get('https://viacep.com.br/ws/' + zipcode + '/json')
-    .then(function (response) {
-        if (response.data.erro) {
-            throw new Error ('CEP inválido')
+            var zipcode = zipCodeField.value
+
+            zipcode = zipcode.replace(' ', '')
+            zipcode = zipcode.replace('.', '')
+            zipcode = zipcode.trim()
+
+            axios
+            .get('https://viacep.com.br/ws/' + zipcode + '/json')
+            .then(function (response) {
+                if (response.data.erro) {
+                    throw new Error ('CEP inválido')
+                }
+                content.innerHTML = ''
+                createLine(response.data.logradouro + ', ' + response.data.bairro + ' - ' + response.data.localidade + '/' + response.data.uf)
+            })
+            .catch(function (error) {
+                console.log(error)
+                content.innerHTML = ''
+                createLine('Ops, algo deu errado!')
+            })
         }
-        content.innerHTML = ''
-        createLine(response.data.logradouro + ', ' + response.data.bairro + ' - ' + response.data.localidade + '/' + response.data.uf)
-    })
-    .catch(function (error) {
-        console.log(error)
-        content.innerHTML = ''
-        createLine('Ops, algo deu errado!')
-    })
+    });
+
+    submitButton.addEventListener("click", function () {
+        event.preventDefault()
+
+            var zipcode = zipCodeField.value
+
+            zipcode = zipcode.replace(' ', '')
+            zipcode = zipcode.replace('.', '')
+            zipcode = zipcode.trim()
+
+            axios
+            .get('https://viacep.com.br/ws/' + zipcode + '/json')
+            .then(function (response) {
+                if (response.data.erro) {
+                    throw new Error ('CEP inválido')
+                }
+                content.innerHTML = ''
+                createLine(response.data.logradouro + ', ' + response.data.bairro + ' - ' + response.data.localidade + '/' + response.data.uf)
+            })
+            .catch(function (error) {
+                console.log(error)
+                content.innerHTML = ''
+                createLine('Ops, algo deu errado!')
+            })
+    });
 }
 
 function createLine (text) {
